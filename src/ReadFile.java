31a43debd7d06dd220f06cda89907439ba4c9432
JOAQUIMMendes <JOAQUIMMendes@PT056986.home>
2018-06-19 22:14:48 +0100

    Criação da classe MyProgram e ReadFile

---
+++ src/ReadFile.java
@@ -0,0 +1,11 @@
+
+public class ReadFile {
+
+    int age = 10;    
+    String model = "Dukemobile";  
+
+    String getDescription() {       
+        //select one of the options to insert here 
+        return model + ": " + age + " years old";
+    }
+}
